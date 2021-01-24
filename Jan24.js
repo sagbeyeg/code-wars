f53e3c48544124e16b2f3717548fc2bb0ab89749
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-24 15:47:24 -0500

    dubstep

--- Jan24.js
+++ Jan24.js
@@ -1,1 +1,16 @@
 //----FIND THE ODD INTEGER----//
+// Input
+// The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
+// Output
+// Return the words of the initial song that Polycarpus used to make a dubsteb remix. Separate the words with a space.
+// Examples
+// =>  WE ARE THE CHAMPIONS MY FRIEND
+
+const songDecoder = song => {
+  return song.replace(/WUB/g, " ").split(" ").filter(obj => obj != "").join(" ")
+}
+// TEST
+// console.log(songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"))
+
+// BEST PRACTICES
+// const songDecoder = song => song.replace(/(WUB)+/g, ' ').trim();

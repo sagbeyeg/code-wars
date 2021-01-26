226aad30638f306616642cbf526cf9b49d177df8
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-26 14:06:10 -0500

    rot13

---
+++ Jan26.js
@@ -0,0 +1,8 @@
+//----ROT13----//
+// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+const rot13 = message => {
+  //your code here
+}
+
+console.log(rot13("Test"))

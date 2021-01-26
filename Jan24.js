e1e837d89fc5cf182c6f86d9e994d6f41f5e92ba
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-25 20:10:49 -0500

    pagination helper

--- Jan24.js
+++ Jan24.js
@@ -1,4 +1,4 @@
-//----FIND THE ODD INTEGER----//
+//----DUBSTEP----//
 // Input
 // The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
 // Output
---
+++ Jan25.js
@@ -0,0 +1,49 @@
+//----PAGINATION HELPER----//
+
+// The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+// The following are some examples of how this class is used:
+
+// var helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
+// helper.pageCount(); //should == 2
+// helper.itemCount(); //should == 6
+// helper.pageItemCount(0); //should == 4
+// helper.pageItemCount(1); // last page - should == 2
+// helper.pageItemCount(2); // should == -1 since the page is invalid
+
+// // pageIndex takes an item index and returns the page that it belongs on
+// helper.pageIndex(5); //should == 1 (zero based index)
+// helper.pageIndex(2); //should == 0
+// helper.pageIndex(20); //should == -1
+// helper.pageIndex(-10); //should == -1
+
+
+// TODO: complete this object/class
+
+// The constructor takes in an array of items and a integer indicating how many
+// items fit within a single page
+function PaginationHelper(collection, itemsPerPage){
+  
+}
+
+// returns the number of items within the entire collection
+PaginationHelper.prototype.itemCount = function() {
+  
+}
+
+// returns the number of pages
+PaginationHelper.prototype.pageCount = function() {
+  
+}
+
+// returns the number of items on the current page. page_index is zero based.
+// this method should return -1 for pageIndex values that are out of range
+PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+  
+}
+
+// determines what page an item is on. Zero based indexes
+// this method should return -1 for itemIndex values that are out of range
+PaginationHelper.prototype.pageIndex = function(itemIndex) {
+  
+}

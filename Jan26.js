c92fff5f090dfc677f7a27619ab4814a446938bc
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-26 14:28:08 -0500

    rot13 best practices

--- Jan26.js
+++ Jan26.js
@@ -61,3 +61,10 @@ const rot13 = message => {
 }
 //TEST
 console.log(rot13("Ruby is cool!")) //should return Ehol vf pbby!
+
+// BEST PRACTICES
+// function rot13(message) {
+//   var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//   var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+//   return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+// }

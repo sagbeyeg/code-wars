7ba9d1d7efbad60f6036893843c27353a00d0942
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-25 21:14:45 -0500

    roman numerals decoder

--- Jan25.js
+++ Jan25.js
@@ -1,3 +1,26 @@
+//----ROMAN NUMERALS DECODER----//
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// Help:
+
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+
+const solution = roman => {
+  
+}
+//TEST
+console.log(solution('XXI')); // should return 21
+
+
 //----PAGINATION HELPER----//
 
 // The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.

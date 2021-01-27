2d2fa3d73a43a12dce8464b16b1cf631e39f5314
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-27 14:52:41 -0500

    pagination helper solution

--- Jan25.js
+++ Jan25.js
@@ -14,13 +14,13 @@
 // D          500
 // M          1,000
 const value = {
-  "I" : 1,
-  "V" : 5,
-  "X" : 10,
-  "L" : 50,
-  "C" : 100,
-  "D" : 500,
-  "M" : 1000
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
 }
 const solution = roman => {
   var split = roman.split("")
@@ -32,7 +32,7 @@ const solution = roman => {
   return sum
 }
 //TEST
-console.log(solution('XIX')); // should return 4
+// console.log(solution('XIX')); // should return 4
 
 
 //----PAGINATION HELPER----//
@@ -59,28 +59,71 @@ console.log(solution('XIX')); // should return 4
 
 // The constructor takes in an array of items and a integer indicating how many
 // items fit within a single page
-function PaginationHelper(collection, itemsPerPage){
-  
+function PaginationHelper(collection, itemsPerPage) {
+  this.group = collection
+  this.pageItems = itemsPerPage
 }
 
 // returns the number of items within the entire collection
-PaginationHelper.prototype.itemCount = function() {
-  
+PaginationHelper.prototype.itemCount = function () {
+  return this.group.length
 }
 
+
 // returns the number of pages
-PaginationHelper.prototype.pageCount = function() {
-  
+PaginationHelper.prototype.pageCount = function () {
+  return Math.ceil(this.itemCount() / this.pageItems)
 }
 
 // returns the number of items on the current page. page_index is zero based.
 // this method should return -1 for pageIndex values that are out of range
-PaginationHelper.prototype.pageItemCount = function(pageIndex) {
-  
+PaginationHelper.prototype.pageItemCount = function (pageIndex) {
+  let items = this.itemCount() - pageIndex * this.pageItems
+  console.log("Items:", items)
+  if (items > this.pageItems) {
+    return this.pageItems
+  } else if (items < 1) {
+    return -1
+  } else {
+    return items
+  }
 }
 
 // determines what page an item is on. Zero based indexes
 // this method should return -1 for itemIndex values that are out of range
-PaginationHelper.prototype.pageIndex = function(itemIndex) {
-  
+PaginationHelper.prototype.pageIndex = function (itemIndex) {
+  // console.log(this.itemCount())
+  if (itemIndex > this.itemCount() || itemIndex < 0 || this.itemCount() === 0) {
+    return -1
+  } else if (itemIndex < this.pageItems) {
+    return 0
+  } else {
+    return Math.floor(itemIndex / this.pageItems)
+  }
 }
+
+
+// PaginationHelper.prototype.pageIndex = function (itemIndex) {
+//   // console.log(this.itemCount())
+//   if (this.itemCount() === 0
+//       || itemIndex < 0
+//       || itemIndex > this.itemCount())        return -1;
+//   if (itemIndex === 0
+//       || itemIndex / this.pageItems === 1) return 0;
+//   return Math.floor((itemIndex / this.pageItems));
+
+//   }
+var helper = new PaginationHelper(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'], 4);
+// // pageIndex takes an item index and returns the page that it belongs on
+// console.log(helper.pageIndex()); //should == 1 (zero based index)
+// helper.pageIndex(2); //should == 0
+// helper.pageIndex(20); //should == -1
+// helper.pageIndex(-10); //should == -1
+
+
+// console.log(helper.pageCount()); //should == 2
+// console.log(helper.itemCount()); //should == 6
+// console.log(helper.pageItemCount(0)); //should == 4
+// console.log(helper.pageItemCount(1)); // last page - should == 2
+// console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
+

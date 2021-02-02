8c7bef1a3f6392b8dd1621e6d61e8cd8021bea98
sagbeyeg <agbeyegbesisan@gmail.com>
2021-02-02 12:06:28 -0500

    array.diff solution

--- Jan31.js
+++ Jan31.js
@@ -6,9 +6,18 @@
 // arrayDiff([1,2],[1]) == [2]
 // If a value is present in b, all of its occurrences must be removed from the other:
 
-
 function arrayDiff(a, b) {
-  
+  newArray = a
+  b.map(num => {
+    for(i = 0; i < a.length; i++) {
+      // console.log(i, a[i], b.find(num => num))
+      if (a[i] === num) {
+        newArray.splice(i, 1);
+        i--;
+      }
+    }
+  })
+  return newArray
 }
 //TEST
-console.log(arrayDiff([1,2,2,2,3],[2])) // == [1,3]
+console.log(arrayDiff([1,2,2,2,3],[])) // == [1,3]

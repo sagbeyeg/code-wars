924dc7f6b95309a82ed7f36b72c55450da2b1891
sagbeyeg <agbeyegbesisan@gmail.com>
2021-02-13 12:20:20 -0500

    highest ranked number in an array, created hashmap

--- Feb10.js
+++ Feb10.js
@@ -5,15 +5,22 @@
 
 function highestRank(arr){
   let sorted = arr.sort((a,b) => a-b)
-  // console.log(sorted)
-  highestNum = sorted[sorted.length-1]
+  console.log(sorted)
+  highestNum = 0
   // console.log(highestNum)
-  
+  frequency = {}
   for (let i=0; i<sorted.length; i++){
-
+    if (frequency[sorted[i]]){
+      frequency[sorted[i]] += 1
+    } else {
+      frequency[sorted[i]] = 1
+    }
   }
+
+  console.log(frequency)
   return highestNum
 }
 //TEST
 
-console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12])) // 12
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 10, 12])) // 12
+// console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10])) // 3

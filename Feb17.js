28b27e850ce5667f48777ebb5c0d9e0e9f75667c
sagbeyeg <agbeyegbesisan@gmail.com>
2021-02-18 13:20:48 -0500

    consecutive strings, accounting for outliers

--- Feb17.js
+++ Feb17.js
@@ -2,7 +2,9 @@
 // You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
 
 function longestConsec(strarr, k) {
-  // your code
+  if (strarr = 0 || k > strarr || k <= 0){
+    return ""
+  }
 }
 
 

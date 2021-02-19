507baf68efe1c004563facd036a0974767dfc117
sagbeyeg <agbeyegbesisan@gmail.com>
2021-02-19 13:34:45 -0500

    consecutive strings, solution for k=2

--- Feb17.js
+++ Feb17.js
@@ -1,18 +1,24 @@
 //----CONSECUTIVE STRINGS----//
 // You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
 
-function longestConsec(strarr, k) {
-  if (strarr = 0 || k > strarr || k <= 0){
+function longestConsec(arr, k) {
+  let newWords = []
+  if (arr.length === 0 || k > arr.length || k <= 0){
     return ""
+  } else {
+    for(let i = 0; i< arr.length - 1; i++){
+      newWords.push(`${arr[i]}${arr[i+1]}`)
+    }
   }
+  return newWords.reduce((a,b) => a.length >= b.length ? a : b)
 }
 
 
 // Examples:
-let strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
-let k = 2
+// let strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
+// let k = 2
 
-console.log(longestConsec(strarr, k))
+console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2)) //"folingtrashy"
 
 // Concatenate the consecutive strings of strarr by 2, we get:
 
@@ -27,7 +33,7 @@ console.log(longestConsec(strarr, k))
 // longest_consec(strarr, 2) should return "folingtrashy".
 
 // In the same way:
-// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2)) // --> "abigailtheta"
 // n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
 
 // Note

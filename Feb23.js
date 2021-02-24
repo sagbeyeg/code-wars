c8fa6a4cfe7547a206b3263149c258d1ee8aaff9
sagbeyeg <agbeyegbesisan@gmail.com>
2021-02-24 15:32:38 -0500

    the deaf rats of hamelin solution

--- Feb23.js
+++ Feb23.js
@@ -9,19 +9,14 @@
 
 const countDeafRats = town => {
   let split = town.replace(/[ ]/g, "").split("P")
-  console.log("split:", split)
   const putSpace = item => item.replace(/(.{2})/g, "$1 ")
-  let leftSplit = putSpace(split[0]).match(/O~/g || []).length
-  console.log("left split:", leftSplit)
-  let rightSplit = putSpace(split[1]).match(/~O/g || []).length
-  console.log("right split:", rightSplit)
-
+  return (putSpace(split[0]).match(/O~/g) || []).length + (putSpace(split[1]).match(/~O/g) || []).length
 }
 
 // Example
-// console.log(countDeafRats("~O~O~O~O P")) // 0
+console.log(countDeafRats("~O~O~O~O P")) // 0
 // ex1 ~O~O~O~O P has 0 deaf rats
-// console.log(countDeafRats("P O~ O~ ~O O~")) // 1
+console.log(countDeafRats("P O~ O~ ~O O~")) // 1
 // ex2 P O~ O~ ~O O~ has 1 deaf rat
 console.log(countDeafRats("~O~O~O~OP~O~OO~")) // 2
 // ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats

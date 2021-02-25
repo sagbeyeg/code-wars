abd6db0ec071991af8476ec675e2e2cfecc00857
sagbeyeg <agbeyegbesisan@gmail.com>
2021-02-25 15:42:14 -0500

    math issues

---
+++ Feb25.js
@@ -0,0 +1,30 @@
+//----MATH ISSUES----//
+// Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+// Here is a list of functions, we need:
+
+// Math.round() -> returns the value of a number rounded to the nearest integer.
+// Math.ceil() -> Rounds a number upward to its nearest integer
+// Math.floor() -> Rounds a number downward to its nearest integer
+
+Math.round = function(number) {
+
+};
+
+Math.ceil = function(number) {
+
+};
+
+Math.floor = function(number) {
+
+};
+
+//TEST
+console.log(Math.round(0.4)) // => 0
+console.log(Math.round(0.5)) // => 1
+
+console.log(Math.ceil(0.4))// => 1
+console.log(Math.ceil(0.5))// => 1
+
+console.log(Math.floor(0.4)) // => 0
+console.log(Math.floor(0.5)) // => 0

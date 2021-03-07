73847ca66e2fe0ddbcfb6dabbae7b9b6c24fec2b
sagbeyeg <agbeyegbesisan@gmail.com>
2021-03-06 21:24:09 -0500

    created numToWord hashmap

--- Mar05.js
+++ Mar05.js
@@ -1,16 +1,13 @@
 //----SORT - ONE, THREE, TWO----//
 //Hey You !
 // Sort these integers for me ...
-
 // By name ...
 
 // Do it now !
 
 // Input
 // Range is 0-999
-
 // There may be duplicates
-
 // The array may be empty
 
 // Example
@@ -22,16 +19,33 @@
 // Don't pack words together:
 
 // e.g. 99 may be "ninety nine" or "ninety-nine"; but not "ninetynine"
-
 // e.g 101 may be "one hundred one" or "one hundred and one"; but not "onehundredone"
-
 // Don't fret about formatting rules, because if rules are consistently applied it has no effect anyway:
-
 // e.g. "one hundred one", "one hundred two"; is same order as "one hundred and one", "one hundred and two"
-
 // e.g. "ninety eight", "ninety nine"; is same order as "ninety-eight", "ninety-nine"
-
 // ```if:c
-
 // For C code the input array may be NULL. The return value is freed if non-NULL.
 
+function sortByName(ary) {
+  // create hashmap
+  const numToWords = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    15: "fifteen",
+    20: "twenty",
+    30: "thirty",
+    50: "fifty"
+  }
+  return []
+}

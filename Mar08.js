f16666b4fa3be952afdf260fc740aad4d9ec5737
sagbeyeg <agbeyegbesisan@gmail.com>
2021-03-08 21:41:05 -0500

    pre fizz buzz workout

---
+++ Mar08.js
@@ -0,0 +1,13 @@
+//----Pre-FizzBuzz Workout #1----//
+// This is the first step to understanding FizzBuzz.
+// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+// Your expected output is an array of positive integers from 1 to n (inclusive).
+// Your job is to write an algorithm that gets you from the input to the output.
+
+function preFizz(n) {
+
+}
+
+console.log(preFizz(1))
+console.log(preFizz(100))
+console.log(preFizz(10))
